6e2b7760d5652455317090431371bc69e775e9f3
tungyao <tungyao@qq.com>
2024-07-10 15:04:56 +0800

    add i2c lib

---
+++ main/myi2c.h
@@ -0,0 +1,25 @@
+//
+// Created by Administrator on 2024/7/10.
+//
+
+#ifndef MYI2C_H
+#define MYI2C_H
+
+#pragma once
+#include "esp_err.h"
+
+
+
+#define I2C_MASTER_SCL_IO           CONFIG_I2C_MASTER_SCL      /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO           CONFIG_I2C_MASTER_SDA      /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
+#define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_TIMEOUT_MS       1000
+
+extern esp_err_t i2c_master_init(void);
+
+
+
+#endif //MYI2C_H
